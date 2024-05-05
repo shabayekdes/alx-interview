2adfb88c7c23fbb89ca8ee790a6dbcf78c460ede
Esmail Shabayek <esmail.shabayek@gmail.com>
2024-05-06 01:27:46 +0300

    Fix

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -4,34 +4,49 @@
 
 import sys
 
-cache = {'200': 0, '301': 0, '400': 0, '401': 0,
-         '403': 0, '404': 0, '405': 0, '500': 0}
-total_size = 0
-counter = 0
 
-try:
-    for line in sys.stdin:
-        line_list = line.split(" ")
-        if len(line_list) > 4:
-            code = line_list[-2]
-            size = int(line_list[-1])
-            if code in cache.keys():
-                cache[code] += 1
-            total_size += size
-            counter += 1
+def process_logs():
+    cache = {
+        '200': 0,
+        '301': 0,
+        '400': 0,
+        '401': 0,
+        '403': 0,
+        '404': 0,
+        '405': 0,
+        '500': 0
+    }
+    total_size = 0
+    counter = 0
 
-        if counter == 10:
-            counter = 0
-            print('File size: {}'.format(total_size))
-            for key, value in sorted(cache.items()):
-                if value != 0:
-                    print('{}: {}'.format(key, value))
+    try:
+        for line in sys.stdin:
+            line_list = line.split(" ")
+            if len(line_list) > 4:
+                code = line_list[-2]
+                size = int(line_list[-1])
+                if code in cache.keys():
+                    cache[code] += 1
+                total_size += size
+                counter += 1
 
-except Exception as err:
-    pass
+            if counter == 10:
+                counter = 0
+                print_stats(total_size, cache)
 
-finally:
-    print('File size: {}'.format(total_size))
-    for key, value in sorted(cache.items()):
+    except Exception as err:
+        pass
+
+    finally:
+        print_stats(total_size, cache)
+
+
+def print_stats(file_size_total, status_code_counts):
+    print('File size: {}'.format(file_size_total))
+    for key, value in sorted(status_code_counts.items()):
         if value != 0:
             print('{}: {}'.format(key, value))
+
+
+if __name__ == "__main__":
+    process_logs()
